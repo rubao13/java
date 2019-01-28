834d17894364d32a8d5afe4bef0c5a00dab98498
Rubens <rubens@vish.be>
2019-01-28 16:43:36 -0200

    v1.1

---
+++ compilaroda.sh
@@ -0,0 +1,11 @@
+cd /opt/vertigo
+
+javac Hello.java
+
+echo Compilous
+
+ls -lhart
+
+cd /opt/
+
+java vertigo/Hello
---
+++ vertigo/Hello.java
@@ -0,0 +1,11 @@
+package vertigo;
+
+        public class Hello {
+
+                public static void main (String args[]){
+                        System.out.println("Olá Farofa ;-) ");
+                }
+        }
+
+
+
